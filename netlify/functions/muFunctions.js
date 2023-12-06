8acaa30f3d43ee861cdcf0de1b95b24438c9b9ca
Babar-Jawad-Anjum <89509895+Babar-Jawad-Anjum@users.noreply.github.com>
2023-12-06 23:38:36 +0500

    changes

--- netlify/functions/muFunctions.js
+++ netlify/functions/muFunctions.js
@@ -2,4 +2,5 @@
 const app = require("../../app"); // Adjust the path accordingly
 const serverless = require("serverless-http");
 
+app.use("/.netlify/functions/api", app.router);
 module.exports.handler = serverless(app);
